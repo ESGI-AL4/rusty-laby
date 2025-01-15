c4dd6755154866f604f1ff096eb5da60eee2c8f2
Baguette42 <87232192+Baguette42@users.noreply.github.com>
2025-01-15 20:07:07 +0100

    Remise au propre, Challange Handler + choix aleatoire

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use serde_json::json;
-use std::io;
+use std::{fmt, io};
 use std::io::{BufRead, Write};
 use std::net::TcpStream;
 use rand::seq::SliceRandom;
@@ -17,6 +17,25 @@ use crate::bin::ascii_utils::{visualize_cells_like_prof, visualize_radar_ascii};
 
 pub const ADDRESS: &str = "localhost:8778";
 
+#[derive(Clone, Debug, PartialEq)]
+enum Direction {
+    FRONT,
+    BACK,
+    LEFT,
+    RIGHT,
+}
+
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let direction_str = match self {
+            Direction::FRONT => "Front",
+            Direction::BACK => "Back",
+            Direction::RIGHT => "Right",
+            Direction::LEFT => "Left",
+        };
+        write!(f, "{}", direction_str)
+    }
+}
 
 // -----------------------------------------------------------------------------
 // GameStreamHandler
@@ -40,21 +59,96 @@ impl GameStreamHandler {
         }
     }
 
-    fn decide_next_action(&self) -> serde_json::Value {
+    /*fn decide_next_action(&self) -> serde_json::Value {
         let mut rng = rand::thread_rng();
         let default_direction = "Front".to_string();
         let random_direction = self.directions.choose(&mut rng).unwrap_or(&default_direction);
         println!("Decide next action: {}", random_direction);
 
         json!({ "MoveTo": random_direction })
+    }*/
+
+
+    fn possible_action(&self, radar_view: String) -> Vec<Direction> {
+        let decoded_radar_view = decode_radar_view(radar_view.replace("\"", ""));
+        let mut actions = Vec::new();
+
+        match decoded_radar_view {
+            Ok(radar_view) => {
+                let walls = [
+                    (Direction::FRONT, radar_view.horizontal_walls[4].clone()),
+                    (Direction::BACK, radar_view.horizontal_walls[7].clone()),
+                    (Direction::LEFT, radar_view.vertical_walls[5].clone()),
+                    (Direction::RIGHT, radar_view.vertical_walls[6].clone()),
+                ];
+
+                for (direction, wall) in &walls {
+                    if wall == &Wall::Open {
+                        actions.push(direction.clone()); // Use `.clone()` here
+                    }
+                }
+            }
+            Err(e) => {
+                println!("Error decoding radar view: {}", e);
+                actions.push(Direction::FRONT);
+            }
+
+        }
+
+        actions
     }
 
-    fn receive_and_parse_message(&mut self) -> io::Result<serde_json::Value> {
-        let msg = network::receive_message(&mut self.stream)?;
-        println!("Server - received message: {}", msg);
+    fn decide_next_action(&self, radar_view: String) -> serde_json::Value {
+        let possible_actions = self.possible_action(radar_view.clone());
 
-        let parsed_msg = json_utils::parse_json(&msg)?;
-        Ok(parsed_msg)
+        // let mut rng = rng();
+        //
+        // let default_direction = Direction::FRONT;
+        // let binding = &default_direction;
+        //
+        // let random_direction = possible_actions
+        //     .choose(&mut rng)
+        //     .unwrap_or(&binding);
+
+        println!("Actions: {:?}", possible_actions);
+        println!("RadarView: {:?}", radar_view);
+
+
+        let action = if possible_actions.contains(&Direction::FRONT) {
+            Direction::FRONT
+        } else if possible_actions.contains(&Direction::LEFT) {
+            Direction::LEFT
+        } else if possible_actions.contains(&Direction::BACK) {
+            Direction::BACK
+        } else {
+            Direction::RIGHT
+        };
+
+
+        println!("Possible actions are: {:?} for radar view : {}", possible_actions, radar_view);
+        println!("Decide next action: {:?} ", action);
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Échec de la lecture");
+
+        json!({"MoveTo": action.to_string()});
+
+        fn decide_next_action(&self) -> serde_json::Value {
+            let mut rng = rand::thread_rng();
+            let default_direction = "Front".to_string();
+            let random_direction = self.directions.choose(&mut rng).unwrap_or(&default_direction);
+            println!("Decide next action: {}", random_direction);
+
+            json!({ "MoveTo": random_direction })
+        }
+
+        fn receive_and_parse_message(&mut self) -> io::Result<serde_json::Value> {
+            let msg = network::receive_message(&mut self.stream)?;
+            println!("Server - received message: {}", msg);
+
+            let parsed_msg = json_utils::parse_json(&msg)?;
+            Ok(parsed_msg)
+        }
     }
 
     fn send_action(&mut self, action: &serde_json::Value) -> io::Result<()> {
